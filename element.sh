73005001ef2e2063487536de885cc3dc96eecacd
ajibade-coder <meriesbells1996@gmail.com>
2024-08-27 13:21:31 -0400

    feat: data_result_func

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
 
 
-
+# func to check the  data type in other to get data from database
 DATADERIVE() {
 
   # accessing arg for the function
@@ -13,7 +13,7 @@ DATADERIVE() {
   # checking if arg is a number
   if [[ $argdata =~ ^-?[0-9]+$ ]]
   then
-    DATA=$($PSQL "SELECT * FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $argdata") 
+    DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass,  melting_point_celsius, boiling_point_celsius, type  FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $argdata") 
 
   else
     # getting the length of argdata
@@ -22,14 +22,29 @@ DATADERIVE() {
     # if length is less than 3 likely it a symbol
      if [[ $length -lt 3 ]]
      then
-        DATA=$($PSQL "SELECT * FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$argdata' ") 
+        DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass,  melting_point_celsius, boiling_point_celsius, type FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE symbol = '$argdata' ") 
+
+        # otherwise it a name of element
       else
-        DATA=$($PSQL "SELECT * FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE name = '$argdata' ") 
+        DATA=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass,  melting_point_celsius, boiling_point_celsius, type FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE name = '$argdata' ") 
       fi
 
   fi
 
 
+}
+
+# func to give a result based on the data gotten
+DATA_RESULT() {
+  # if data cant be found ....
+  if [[ -z $DATA ]]
+  then
+      echo "I could not find that element in the database."
+  else
+      IFS="|" read -r atomic_number symbol name atomic_mass  melting_point_celsius boiling_point_celsius type <<< "$DATA"
+
+      echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
+  fi
 }
 
 
@@ -38,9 +53,6 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 else
-  ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number='1'")
-  IFS="|" read -r NUM SIGN NAME <<< "$ELEMENT"
-  echo $NAME
+  
 
-  echo "$($PSQL "SELECT * FROM elements JOIN  properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = 1")"
 fi
